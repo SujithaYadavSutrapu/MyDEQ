de07aacfa6b3fc29f8b6cf231ad4ff332dc07419
SrilekhaMS <156909303+SrilekhaMS@users.noreply.github.com>
2024-03-11 14:50:44 +0530

    first message

--- frontend/mydeq/src/app/app.component.ts
+++ frontend/mydeq/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, CommonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mydeq';
+
+
 }
